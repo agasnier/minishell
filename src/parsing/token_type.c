a846b788f478cd2e2c24f05b4c243f0feec62c72
algasnie <gasnieralexandre@icloud.com>
2026-03-06 14:59:42 +0100

    handle heredoc into fork and child to handle signal

--- src/parsing/token_type.c
+++ src/parsing/token_type.c
@@ -6,28 +6,26 @@
 /*   By: algasnie <algasnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 14:28:53 by algasnie          #+#    #+#             */
-/*   Updated: 2026/03/06 13:55:04 by algasnie         ###   ########.fr       */
+/*   Updated: 2026/03/06 14:56:43 by algasnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	handle_heredoc(t_minishell *minishell, t_cmd *cmd, t_token *token_next)
+static void	handle_heredoc_child(t_minishell *minishell, t_token *token_next, int fd[2])
 {
 	char	*line;
-	int		fd[2];
 
 	signal(SIGINT, SIG_DFL);
-	if (pipe(fd) == -1)
-	{
-		perror("minishell:");
-		return (1);
-	}
+	close(fd[0]);
 	while (1)
 	{
 		line = readline("> ");
 		if (!line)
+		{
+			ft_printf(STDERR_FILENO, "minishell: warning: here-document delimited by end-of-file (wanted `%s')\n", token_next->token);
 			break ;
+		}
 		if (ft_strcmp(line, token_next->token) == 0)
 		{
 			free(line);
@@ -39,6 +37,30 @@ static int	handle_heredoc(t_minishell *minishell, t_cmd *cmd, t_token *token_nex
 		free(line);
 	}
 	close(fd[1]);
+	exit(0);
+}
+
+static int	handle_heredoc(t_minishell *minishell, t_cmd *cmd, t_token *token_next)
+{
+	int		fd[2];
+	pid_t	pid;
+	int		status;
+
+	if (pipe(fd) == -1)
+		return (1);
+	pid = fork();
+	if (pid == 0)
+		handle_heredoc_child(minishell, token_next, fd);
+	close(fd[1]);
+	signal(SIGINT, SIG_IGN);
+	waitpid(pid, &status, 0);
+	signal(SIGINT, handle_signal);
+	if (WIFSIGNALED(status))
+	{
+		close(fd[0]);
+		write(1, "\n", 1);
+		return (1);
+	}
 	if (cmd->fd_in > 2)
 		close(cmd->fd_in);
 	cmd->fd_in = fd[0];
